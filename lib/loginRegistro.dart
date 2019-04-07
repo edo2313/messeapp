b7bbe347937b5a999bc897f538a32b1d97edc4c9
pietrocipriani <47864521+pietrocipriani@users.noreply.github.com>
2019-04-07 16:20:34 +0200

    login implementation

--- lib/loginRegistro.dart
+++ lib/loginRegistro.dart
@@ -1,19 +1,51 @@
-
+import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:html/dom.dart' as dom;
+import 'package:html/parser.dart' show parse;
 
 class LoginRegistro {
 
 
-  static void makeLogin (String username, String password){
+  static Future<String> makeLogin (BuildContext context, String username, String password) async {
     Map<String, String> data = Map();
     data.putIfAbsent("uid", () => username);
     data.putIfAbsent("pwd", () => password);
     
-    http.post('https://web.spaggiari.eu/auth-p7/app/default/AuthApi4.php?a=aLoginPwd', body: data)
-        .then((http.Response r) {
-          //TODO: analizzare il body di r (JSON) e prendere il valore 'loggedIn'
-          print(r.body);
-        });
-    
+    http.Response r = await http.post('https://web.spaggiari.eu/auth-p7/app/default/AuthApi4.php?a=aLoginPwd', body: data);
+    print (r.headers);
+    if (r.statusCode != 200){
+      /*Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Errore ${r.statusCode}!"),
+            action: SnackBarAction(label: "RIPROVA", onPressed: () => makeLogin(context, username, password),),
+          )
+      );*/
+      return null;
+    }
+    var json = jsonDecode(r.body);
+    assert (json is Map);
+    bool logged = json.values.elementAt(1).values.elementAt(0).values.elementAt(1);
+    if (!logged) {
+      /*Scaffold.of(context).showSnackBar(
+        SnackBar(content: Text(
+            json.values.elementAt(1).values.elementAt(0).values.elementAt(4).toString()
+        ))
+      );*/
+      return null ;
+    }
+    String setCookies = r.headers.values.elementAt(2);
+    print (setCookies);
+    //set_cookies = set_cookies.substring(0, 42);
+    setCookies = setCookies.substring(0, 42);
+    print (setCookies);
+
+
+    /* test: not working
+    r = await http.get('https://web.spaggiari.eu/fml/app/default/regclasse_lezioni_xstudenti.php?action=loadLezioni&materia=201690&autori_id=505072', headers: {'Cookies': setCookies});
+    dom.Document doc = parse(r.body);
+    print (doc.children.elementAt(0).children.elementAt(1).children.elementAt(0).innerHtml);
+    */
+    return setCookies;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'loginRegistro.dart';
 
 void main(){
   // test temporaneo
-  LoginRegistro.makeLogin('', '');
+  //LoginRegistro.makeLogin('aaa', 'bbb');
 
   runApp(MyApp());
 }
@@ -39,18 +39,80 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Center(child: Text(widget.title))
       ),
-      body: Center(
-        child: Text ("PAGINA")
-      ),
+      body: buildBody(context),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: index,
+        type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.vpn_key), title: Text('REGISTRO')),
           BottomNavigationBarItem(icon: Icon(Icons.calendar_today), title: Text('AGENDA')),
           BottomNavigationBarItem(icon: Icon(Icons.access_time), title: Text('ORARI')),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("IMPOSTAZIONI"))
         ],
         onTap: (int i) => setState(() => index = i),
       ),
+    );
+  }
+  Widget buildBody (BuildContext context) {
+    switch (index){
+      case 0:
+        return buildLogin(context);
+      default:
+        return Center(child: Text("PAGINA"),);
+    }
+  }
+  Widget buildLogin (BuildContext context) {
+    TextEditingController unameController = TextEditingController();
+    TextEditingController pwordController = TextEditingController();
+    TextFormField uname = TextFormField(
+      controller: unameController,
+      keyboardType: TextInputType.text,
+      autofocus: false,
+      decoration: InputDecoration(
+          hintText: "username",
+          contentPadding: EdgeInsets.all(10),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+          )
+      ),
+    );
+    TextFormField pword = TextFormField(
+      controller: pwordController,
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      decoration: InputDecoration(
+          hintText: "password",
+          contentPadding: EdgeInsets.all(10),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30)
+          )
+      ),
+    );
+
+    Material btn = Material(
+      borderRadius: BorderRadius.circular(30),
+      shadowColor: Colors.black54,
+      color: Colors.lightGreen[900],
+      elevation: 5.0,
+      child: MaterialButton(
+        minWidth: 200.0,
+        height: 42.0,
+        onPressed: (){LoginRegistro.makeLogin(context, unameController.text, pwordController.text);},
+        //color: Colors.lightGreen[900],
+        child: Text("LOGIN", style: TextStyle(color: Colors.white,),),
+      ),
+    );
+
+    return ListView(
+      shrinkWrap: true,
+      padding: EdgeInsets.all(20),
+      children: <Widget>[
+        uname,
+        SizedBox(height: 8),
+        pword,
+        SizedBox(height: 24),
+        btn,
+      ],
     );
   }
 }
